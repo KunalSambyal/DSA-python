fa564d8958af0e57cf8f0e4ca558fae3cce445c0
Kunal Sambyal <kunalsambyal9628@gmail.com>
2026-07-01 16:44:46 +0530

    Bubble Sort Algorithm

---
+++ Algorithms/sorting/bubble_sort.py
@@ -0,0 +1,19 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n-1, -1, -1):
+        did_swap = False
+
+        for j in range(i):
+            if (arr[j] > arr[j+1]):
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                did_swap = True
+
+        if not did_swap:
+            break
+
+    return arr
+
+arr = [64, -34, 25, 12, 22, 11, 90]
+print("Array before sort:", arr)
+print("Array after sort:", bubble_sort(arr))
+
